b69d0fec49ec5f2fd6b81d49edce17d6443bd51b
salok1 <nicolas93pelletier@gmail.com>
2025-11-29 22:22:18 +0100

    kok

--- __init__.py
+++ __init__.py
@@ -1,3 +1,6 @@
-from .heston_model.heston_data_builder import get_heston_dataset
-
-__all__ = ["get_heston_dataset"]
+# Expose get_heston_dataset when imported as a package; tolerate direct execution contexts.
+try:
+    from heston_model.heston_data_builder import get_heston_dataset  # type: ignore
+    __all__ = ["get_heston_dataset"]
+except Exception:  # pragma: no cover - allow non-package import contexts
+    __all__ = []
